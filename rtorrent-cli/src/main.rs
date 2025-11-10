3ab73185149829d9f571faf80f7ec2a98e082539
Frank Albella <frankalbella@gmail.com>
2025-11-10 15:51:38 -0500

    rtorrent-cli: add argument parsing

--- rtorrent-cli/src/main.rs
+++ rtorrent-cli/src/main.rs
@@ -1,1 +1,25 @@
-fn main() {}
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+#[clap(rename_all = "snake_case")]
+enum Command {
+    Add { value: String },
+    Remove { value: String },
+    Info { value: String },
+    List { value: String },
+}
+
+fn main() {
+    let args = Args::parse();
+
+    match args.command {
+        x => todo!("{x:#?}"),
+    }
+}
